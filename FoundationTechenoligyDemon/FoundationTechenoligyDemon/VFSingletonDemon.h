666ce2229835eb1cf5369a8637fca17effe72499
victorfun <victorfun8@gmail.com>
2015-01-26 21:32:36 +0800

    singleton demon add define at .pch

---
+++ FoundationTechenoligyDemon/FoundationTechenoligyDemon/VFSingletonDemon.h
@@ -0,0 +1,15 @@
+//
+//  VFSingletonDemon.h
+//  FoundationTechenoligyDemon
+//
+//  Created by vic on 1/26/15.
+//  Copyright (c) 2015 nero. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VFSingletonDemon : NSObject
+
+DEFINE_SINGLETON_FOR_HEADER(VFSingletonDemon);
+
+@end
---
+++ FoundationTechenoligyDemon/FoundationTechenoligyDemon/VFSingletonDemon.m
@@ -0,0 +1,15 @@
+//
+//  VFSingletonDemon.m
+//  FoundationTechenoligyDemon
+//
+//  Created by vic on 1/26/15.
+//  Copyright (c) 2015 nero. All rights reserved.
+//
+
+#import "VFSingletonDemon.h"
+
+@implementation VFSingletonDemon
+
+DEFINE_SINGLETON_FOR_CLASS(VFSingletonDemon);
+
+@end
