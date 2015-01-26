73dc318a9661cc6308d8a79297870767417c5c0e
vic_ <victor@victorfuns-imac.lan>
2015-01-26 17:51:45 +0800

    add data type

---
+++ FoundationTechenoligyDemon/FoundationTechenoligyDemon/VFDataType.h
@@ -0,0 +1,13 @@
+//
+//  VFDataType.h
+//  FoundationTechenoligyDemon
+//
+//  Created by victor on 1/26/15.
+//  Copyright (c) 2015 nero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VFDataType : UIViewController
+
+@end
