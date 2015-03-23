314d15d318a41113a656a68267a9fceaf29d8d10
victorfun <victorfun8@gmail.com>
2015-03-23 22:59:14 +0800

    add

---
+++ FoundationTechenoligyDemon/FoundationTechenoligyDemon/VFArrayDemon.h
@@ -0,0 +1,13 @@
+//
+//  VFArrayDemon.h
+//  FoundationTechenoligyDemon
+//
+//  Created by vic on 3/23/15.
+//  Copyright (c) 2015 nero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VFArrayDemon : UIViewController
+
+@end
---
+++ FoundationTechenoligyDemon/FoundationTechenoligyDemon/VFArrayDemon.m
@@ -0,0 +1,63 @@
+//
+//  VFArrayDemon.m
+//  FoundationTechenoligyDemon
+//
+//  Created by vic on 3/23/15.
+//  Copyright (c) 2015 nero. All rights reserved.
+//
+
+#import "VFArrayDemon.h"
+
+@interface VFArrayDemon ()
+{
+    UIButton *bt;
+}
+
+@end
+
+@implementation VFArrayDemon
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    bt = [[UIButton alloc] initWithFrame:CGRectMake(40, 40, 120, 40)];
+    bt.backgroundColor = [UIColor redColor];
+    bt.titleLabel.text = @"hit me!";
+    bt.titleLabel.textColor = [UIColor grayColor];
+    [bt addTarget:self action:@selector(onBtClick:) forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.view addSubview:bt];
+}
+
+-(void)onBtClick: (id)sender
+{
+    [self saveData2Plist];
+}
+
+-(void)saveData2Plist
+{
+    NSMutableDictionary *dic = [[NSMutableDictionary alloc] init];
+    
+    [dic setObject:@"victor" forKey:@"name"];
+    [dic setValue:@"28" forKey:@"age"];
+    [dic setValue:@"male" forKey:@"genge"];
+    
+    [dic writeToFile:@"~/Library/My.plist" atomically:YES];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
