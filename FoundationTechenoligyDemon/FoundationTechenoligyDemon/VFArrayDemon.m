4345ef7a9a5ab9aef9ab451736435a37dfe1dd90
Vic Fang <vfang@nero.com>
2015-05-19 17:07:31 +0800

    add NSArray NSDictionary

--- FoundationTechenoligyDemon/FoundationTechenoligyDemon/VFArrayDemon.m
+++ FoundationTechenoligyDemon/FoundationTechenoligyDemon/VFArrayDemon.m
@@ -27,16 +27,24 @@
     bt.titleLabel.textColor = [UIColor grayColor];
     [bt addTarget:self action:@selector(onBtClick:) forControlEvents:UIControlEventTouchUpInside];
     
+    tv = [[UITextView alloc] initWithFrame:CGRectMake(40, 80, 120, 120)];
+//    tv.textColor = [UIColor yellowColor];
+    
     [self.view addSubview:bt];
+    [self.view addSubview:tv];
 }
 
 -(void)onBtClick: (id)sender
 {
-    [self saveData2Plist];
+//    [self saveData2Plist];
+//    [self initDictionary];
+    [self initMutableDictionary];
+//    [self initArray];
 }
 
 -(void)saveData2Plist
 {
+//    NSMutableDictionary *dic = [NSMutableDictionary alloc] initwit
 //    NSMutableDictionary *dic = [[NSMutableDictionary alloc] init];
 //    
 //    [dic setObject:@"victor" forKey:@"name"];
@@ -51,19 +59,72 @@
     self->tv.text = [[UIDevice currentDevice] name];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - Dictionary
+- (void)initDictionary
+{
+    NSDictionary *dic;
+    
+    //1
+//    dic = [NSDictionary dictionaryWithContentsOfFile:@""];
+    
+    //2
+//    dic = [NSDictionary dictionaryWithObject:@"victor1" forKey:@"name"];
+    
+    //3
+    dic = [NSDictionary dictionaryWithObjects:[[NSArray alloc] initWithObjects:@"victor2", @"28", @"male", nil] forKeys:[[NSArray alloc] initWithObjects:@"name", @"age", @"gender", nil]];
+    
+    //4
+//    dic = [NSDictionary dictionaryWithObjectsAndKeys:@"victor3", @"name", @"28", @"age", nil];
+    
+    for (NSString *key in [dic keyEnumerator]) {
+        NSLog(@"%@", key);
+        NSLog(@"%@", [dic objectForKey:key]);
+    }
+    
+    NSLog(@"%d", [dic count]);
+    self->tv.text = dic.description;
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)initMutableDictionary
+{
+    NSMutableDictionary *mDic;
+    
+    mDic = [NSMutableDictionary dictionaryWithObjects:[NSArray arrayWithObjects:@"1", @"2", @"3", @"4", nil] forKeys:[NSArray arrayWithObjects:@"First", @"Second", @"Third", @"Forth", nil]];
+    
+    for (NSString *key in [mDic keyEnumerator]) {
+        NSLog(@"%@", key);
+        NSLog(@"%@", [mDic objectForKey:key]);
+    }
+    
+    NSLog(@"%d", [mDic count]);
+    self->tv.text = mDic.description;
 }
-*/
+
+#pragma mark - Array
+- (void)initArray
+{
+    NSArray *array = [NSArray arrayWithObjects:@"1", @"2", @"3", @"4", @"5", @"6", @"7", @"8", @"9", @"10", @"11", nil];
+    
+    for (NSString *value in array) {
+        NSLog(@"%@", value);
+    }
+    
+    NSLog(@"array size is %d", [array count]);
+    
+    self->tv.text = [array description];
+}
+
+- (void)initMutableArray
+{
+    NSMutableArray *array = [NSMutableArray arrayWithObjects:@"1", @"2", @"3", @"4", @"5", @"6", @"7", @"8", @"9", @"10", @"11", nil];
+    
+    for (NSString *value in array) {
+        NSLog(@"%@", value);
+    }
+    
+    NSLog(@"array size is %d", [array count]);
+    
+    self->tv.text = [array description];
+}
 
 @end
