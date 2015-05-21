ae2ed432c9f25d3258180b2e78d89db324f2b7d4
Vic Fang <vfang@nero.com>
2015-05-21 15:21:59 +0800

    add media demon

---
+++ FoundationTechenoligyDemon/FoundationTechenoligyDemon/VFMediaDemon.h
@@ -0,0 +1,13 @@
+//
+//  VFMediaDemon.h
+//  FoundationTechenoligyDemon
+//
+//  Created by victor on 21/5/15.
+//  Copyright (c) 2015 nero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VFMediaDemon : UIViewController
+
+@end
---
+++ FoundationTechenoligyDemon/FoundationTechenoligyDemon/VFMediaDemon.m
@@ -0,0 +1,59 @@
+//
+//  VFMediaDemon.m
+//  FoundationTechenoligyDemon
+//
+//  Created by victor on 21/5/15.
+//  Copyright (c) 2015 nero. All rights reserved.
+//
+
+#import "VFMediaDemon.h"
+#include <AssetsLibrary/AssetsLibrary.h>
+
+@interface VFMediaDemon ()
+{
+    UIButton *bt;
+}
+
+@end
+
+@implementation VFMediaDemon
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+}
+
+-(void)getMediaData
+{
+    ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
+    
+    // Enumerate just the photos and videos group by using ALAssetsGroupSavedPhotos.
+    [library enumerateGroupsWithTypes:ALAssetsGroupSavedPhotos usingBlock:^(ALAssetsGroup *group, BOOL *stop) {
+        
+        // Within the group enumeration block, filter to enumerate just videos.
+        [group setAssetsFilter:[ALAssetsFilter allVideos]];
+        
+        // For this example, we're only interested in the first item.
+        [group enumerateAssetsAtIndexes:[NSIndexSet indexSetWithIndex:0]
+                                options:0
+                             usingBlock:^(ALAsset *alAsset, NSUInteger index, BOOL *innerStop) {
+                                 
+                                 // The end of the enumeration is signaled by asset == nil.
+                                 if (alAsset) {
+                                     ALAssetRepresentation *representation = [alAsset defaultRepresentation];
+                                     NSURL *url = [representation url];
+                                     AVAsset *avAsset = [AVURLAsset URLAssetWithURL:url options:nil];
+                                     // Do something interesting with the AV asset.
+                                 }
+                             }];
+    }
+                         failureBlock: ^(NSError *error) {
+                             // Typically you should handle an error more gracefully than this.
+                             NSLog(@"No groups");
+                         }];
+    
+
+}
+
+@end
